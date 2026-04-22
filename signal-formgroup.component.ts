6f3fe6d1497823e0a859a8fc5865a757e62a458b
Angular2026 <bouhmid@me.com>
2026-04-22 23:56:06 +0200

    Update signal-formgroup.component.ts

--- signal-formgroup.component.ts
+++ signal-formgroup.component.ts
@@ -1,67 +1,3 @@
-copyCounterPartyRating(forceOverwrite = false) {
-  const control = this.ccDecisionsForm()
-    ?.get('ratingCcDecision.approvedCounterpartyRating');
-
-  if (forceOverwrite) {
-    control?.setValue(this.proposedCounterpartyRating());
-  } else {
-    if (control?.value !== this.proposedCounterpartyRating()?.toLocaleUpperCase()) {
-      control?.setValue(this.proposedCounterpartyRating());
-    } else {
-      control?.reset();
-    }
-  }
-}
-
-copyIntrinsicRating(forceOverwrite = false) {
-  const control = this.ccDecisionsForm()
-    ?.get('ratingCcDecision.modelSpecificOverrides.approvedIntrinsicRatingCode');
-
-  if (forceOverwrite) {
-    control?.setValue(this.proposedIntrinsicRating());
-  } else {
-    if (control?.value !== this.proposedIntrinsicRating()?.toLocaleUpperCase()) {
-      control?.setValue(this.proposedIntrinsicRating());
-    } else {
-      control?.reset();
-    }
-  }
-}
-
-copyRatingSuGrr(forceOverwrite = false) {
-  const control = this.ccDecisionsForm()
-    ?.get('ratingCcDecision.modelSpecificOverrides.approvedSuGrrPercentage');
-
-  if (forceOverwrite) {
-    control?.setValue(this.sugrrProposed());
-  } else {
-    if (control?.value !== this.sugrrProposed()) {
-      control?.setValue(this.sugrrProposed());
-    } else {
-      control?.reset();
-    }
-  }
-}
-
-copyLbo(forceOverwrite = false) {
-  if (this.proposedLbo() === undefined) return;
-
-  const control = this.ccDecisionsForm()
-    ?.get('ratingCcDecision.approvedLboFlag');
-
-  if (forceOverwrite) {
-    control?.setValue(this.proposedLbo() === 'YES');
-  } else {
-    const currentValue = control?.value;
-    const expectedValue = this.proposedLbo() === 'YES';
-    if (currentValue === null || currentValue === undefined) {
-      control?.setValue(expectedValue);
-    } else {
-      control?.reset();
-    }
-  }
-}
-
 copyOrRemoveAll() {
   if (this.copyAllRemoveAllButtonLabel === $localize`:@@copyAllLabel:`) {
     this.copyCounterPartyRating(true);
@@ -69,10 +5,30 @@ copyOrRemoveAll() {
     this.copyRatingSuGrr(true);
     this.copyLbo(true);
     this.copyAllRemoveAllButtonLabel = $localize`:@@removeAllLabel:`;
+
   } else if (this.copyAllRemoveAllButtonLabel === $localize`:@@removeAllLabel:`) {
-    this.ccDecisionsForm().get('ratingCcDecision')?.reset();
-    this.ccDecisionsForm().get('ratingCcDecision.modelSpecificOverrides')?.reset();
-    this.ccDecisionsForm().get('ratingCcDecision')?.updateValueAndValidity();
+    // ❌ Remplacer reset() global qui causait l'erreur et ne vidait pas tout
+    // ✅ setValue(null) individuel sur chaque champ approuvé
+    this.ccDecisionsForm()
+      ?.get('ratingCcDecision.approvedCounterpartyRating')
+      ?.setValue(null);
+
+    this.ccDecisionsForm()
+      ?.get('ratingCcDecision.modelSpecificOverrides.approvedIntrinsicRatingCode')
+      ?.setValue(null);
+
+    this.ccDecisionsForm()
+      ?.get('ratingCcDecision.modelSpecificOverrides.approvedSuGrrPercentage')
+      ?.setValue(null);
+
+    this.ccDecisionsForm()
+      ?.get('ratingCcDecision.approvedLboFlag')
+      ?.setValue(null);
+
+    this.ccDecisionsForm()
+      ?.get('ratingCcDecision')
+      ?.updateValueAndValidity();
+
     this.triggerNextRatingDateComputation();
     this.copyAllRemoveAllButtonLabel = $localize`:@@copyAllLabel:`;
   }
