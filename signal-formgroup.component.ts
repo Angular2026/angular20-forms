9ab47725928145385b14a55ae4d5eb1067f0110d
Angular2026 <bouhmid@me.com>
2026-05-15 01:05:03 +0200

    Update signal-formgroup.component.ts

--- signal-formgroup.component.ts
+++ signal-formgroup.component.ts
@@ -1,176 +1,20 @@
-<trans-unit id="sponsorNameLabel" datatype="html">
-  <source>Nom du sponsor</source>
-  <target state="new">Nom du sponsor</target>
-</trans-unit>
+// Sponsor Turnover Closing Date
+const turnoverDateValidators = isCorporate
+  ? [maxMonthsOldValidator('closingDate21Months', 21), ...(hasTurnover ? [Validators.required] : [])]
+  : [];
+c.sponsorTurnoverClosingDate.setValidators(turnoverDateValidators);
+c.sponsorTurnoverClosingDate.updateValueAndValidity({ emitEvent: false });
 
-<trans-unit id="hasExternalRatingLabel" datatype="html">
-  <source>Le sponsor a-t-il une notation externe ?</source>
-  <target state="new">Le sponsor a-t-il une notation externe ?</target>
-</trans-unit>
+// Adjusted Turnover Closing Date
+const adjustedDateValidators = isCorporate
+  ? [maxMonthsOldValidator('closingDate21Months', 21), ...(hasAdjusted ? [Validators.required] : [])]
+  : [];
+c.adjustedSponsorTurnoverClosingDate.setValidators(adjustedDateValidators);
+c.adjustedSponsorTurnoverClosingDate.updateValueAndValidity({ emitEvent: false });
 
-<trans-unit id="externalRatingSourceLabel" datatype="html">
-  <source>Source de la notation externe</source>
-  <target state="new">Source de la notation externe</target>
-</trans-unit>
-
-<trans-unit id="externalRatingLabel" datatype="html">
-  <source>Notation sponsor externe</source>
-  <target state="new">Notation sponsor externe</target>
-</trans-unit>
-
-<trans-unit id="externalRatingDateLabel" datatype="html">
-  <source>Date notation sponsor externe</source>
-  <target state="new">Date notation sponsor externe</target>
-</trans-unit>
-
-<trans-unit id="commentOnExternalDataLabel" datatype="html">
-  <source>Commentaire en cas de données externes</source>
-  <target state="new">Commentaire en cas de données externes</target>
-</trans-unit>
-
-<trans-unit id="sponsorTurnoverLabel" datatype="html">
-  <source>Le chiffre d'affaires du sponsor</source>
-  <target state="new">Le chiffre d'affaires du sponsor</target>
-</trans-unit>
-
-<trans-unit id="sponsorTurnoverClosingDateLabel" datatype="html">
-  <source>Le chiffre d'affaires du sponsor date d'arrêté</source>
-  <target state="new">Le chiffre d'affaires du sponsor date d'arrêté</target>
-</trans-unit>
-
-<trans-unit id="adjustedSponsorTurnoverLabel" datatype="html">
-  <source>Le chiffre d'affaires ajusté du sponsor</source>
-  <target state="new">Le chiffre d'affaires ajusté du sponsor</target>
-</trans-unit>
-
-<trans-unit id="assetsUnderManagementLabel" datatype="html">
-  <source>Actifs sous gestion</source>
-  <target state="new">Actifs sous gestion</target>
-</trans-unit>
-
-<trans-unit id="sponsorInvolvementLabel" datatype="html">
-  <source>Quelle est l'implication du sponsor ?</source>
-  <target state="new">Quelle est l'implication du sponsor ?</target>
-</trans-unit>
-
-<trans-unit id="missingSponsorInvolvementLabel" datatype="html">
-  <source>Item obligatoire manquant : Quelle est l'implication du Sponsor</source>
-  <target state="new">Item obligatoire manquant : Quelle est l'implication du Sponsor</target>
-</trans-unit>
-
-<trans-unit id="sponsorAssessmentLabel" datatype="html">
-  <source>Evaluation et implication du sponsor</source>
-  <target state="new">Evaluation et implication du sponsor</target>
-</trans-unit>
-
-<trans-unit id="closingDate21MonthsWarning" datatype="html">
-  <source>La date d'arrêté saisie dépasse 21 mois - Merci de noter que dans ce cas la pire modalité sera appliquée pour le calcul de la PD.</source>
-  <target state="new">La date d'arrêté saisie dépasse 21 mois - Merci de noter que dans ce cas la pire modalité sera appliquée pour le calcul de la PD.</target>
-</trans-unit>
-
-<trans-unit id="missingSponsorTypeLabel" datatype="html">
-  <source>Le Sponsor ne dispose pas de notation disponible ou valide, veuillez sélectionner le type de sponsor pour continuer le processus d'évaluation de la solidité du sponsor.</source>
-  <target state="new">Le Sponsor ne dispose pas de notation disponible ou valide, veuillez sélectionner le type de sponsor pour continuer le processus d'évaluation de la solidité du sponsor.</target>
-</trans-unit>
-
-<trans-unit id="missingUnitCurrencyClosingDate" datatype="html">
-  <source>Item obligatoire manquant : Unité/ Devise/ Date d'arrêté</source>
-  <target state="new">Item obligatoire manquant : Unité/ Devise/ Date d'arrêté</target>
-</trans-unit>
-
-<trans-unit id="missingComment" datatype="html">
-  <source>Item obligatoire manquant : Commentaire</source>
-  <target state="new">Item obligatoire manquant : Commentaire</target>
-</trans-unit>
-
-
-
-
-
-
-english 
-
-<trans-unit id="sponsorNameLabel" datatype="html">
-  <source>Nom du sponsor</source>
-  <target state="new">Sponsor name</target>
-</trans-unit>
-
-<trans-unit id="hasExternalRatingLabel" datatype="html">
-  <source>Le sponsor a-t-il une notation externe ?</source>
-  <target state="new">Does the sponsor have an external rating?</target>
-</trans-unit>
-
-<trans-unit id="externalRatingSourceLabel" datatype="html">
-  <source>Source de la notation externe</source>
-  <target state="new">External rating source</target>
-</trans-unit>
-
-<trans-unit id="externalRatingLabel" datatype="html">
-  <source>Notation sponsor externe</source>
-  <target state="new">External rating</target>
-</trans-unit>
-
-<trans-unit id="externalRatingDateLabel" datatype="html">
-  <source>Date notation sponsor externe</source>
-  <target state="new">External rating date</target>
-</trans-unit>
-
-<trans-unit id="commentOnExternalDataLabel" datatype="html">
-  <source>Commentaire en cas de données externes</source>
-  <target state="new">Comment on external data</target>
-</trans-unit>
-
-<trans-unit id="sponsorTurnoverLabel" datatype="html">
-  <source>Le chiffre d'affaires du sponsor</source>
-  <target state="new">Sponsor Turnover (Sales)</target>
-</trans-unit>
-
-<trans-unit id="sponsorTurnoverClosingDateLabel" datatype="html">
-  <source>Le chiffre d'affaires du sponsor date d'arrêté</source>
-  <target state="new">Sponsor Turnover (Sales) closing date</target>
-</trans-unit>
-
-<trans-unit id="adjustedSponsorTurnoverLabel" datatype="html">
-  <source>Le chiffre d'affaires ajusté du sponsor</source>
-  <target state="new">Adjusted Sponsor Turnover</target>
-</trans-unit>
-
-<trans-unit id="assetsUnderManagementLabel" datatype="html">
-  <source>Actifs sous gestion</source>
-  <target state="new">Assets under management</target>
-</trans-unit>
-
-<trans-unit id="sponsorInvolvementLabel" datatype="html">
-  <source>Quelle est l'implication du sponsor ?</source>
-  <target state="new">What is the Sponsor's involvement?</target>
-</trans-unit>
-
-<trans-unit id="missingSponsorInvolvementLabel" datatype="html">
-  <source>Item obligatoire manquant : Quelle est l'implication du Sponsor</source>
-  <target state="new">Missing Mandatory item(s): What is the Sponsor's involvement?</target>
-</trans-unit>
-
-<trans-unit id="sponsorAssessmentLabel" datatype="html">
-  <source>Evaluation et implication du sponsor</source>
-  <target state="new">Sponsor assessment and involvement</target>
-</trans-unit>
-
-<trans-unit id="closingDate21MonthsWarning" datatype="html">
-  <source>La date d'arrêté saisie dépasse 21 mois - Merci de noter que dans ce cas la pire modalité sera appliquée pour le calcul de la PD.</source>
-  <target state="new">The inserted Closing date is older than 21 months – Please note that in this case the worst modality will be applied in the PD formula computation.</target>
-</trans-unit>
-
-<trans-unit id="missingSponsorTypeLabel" datatype="html">
-  <source>Le Sponsor ne dispose pas de notation disponible ou valide, veuillez sélectionner le type de sponsor pour continuer le processus d'évaluation de la solidité du sponsor.</source>
-  <target state="new">The Sponsor doesn't have any available or valid rating, please select the type of sponsor to continue the sponsor strength assessment process.</target>
-</trans-unit>
-
-<trans-unit id="missingUnitCurrencyClosingDate" datatype="html">
-  <source>Item obligatoire manquant : Unité/ Devise/ Date d'arrêté</source>
-  <target state="new">Missing Mandatory Item(s): Unit /Currency/ Closing date</target>
-</trans-unit>
-
-<trans-unit id="missingComment" datatype="html">
-  <source>Item obligatoire manquant : Commentaire</source>
-  <target state="new">Missing Mandatory Item(s): Comment</target>
-</trans-unit>
+// AUM Closing Date
+const aumDateValidators = isOther
+  ? [maxMonthsOldValidator('closingDate21Months', 21), ...(hasAUM ? [Validators.required] : [])]
+  : [];
+c.assetsUnderManagementClosingDate.setValidators(aumDateValidators);
+c.assetsUnderManagementClosingDate.updateValueAndValidity({ emitEvent: false });
