0862f8f69d9359915a7f5b7b91a320841aacc3bc
Angular2026 <bouhmid@me.com>
2026-06-02 00:21:41 +0200

    Update formgroup-signals.component.ts

--- formgroup-signals.component.ts
+++ formgroup-signals.component.ts
@@ -1,16 +1,17 @@
 CrfRatingResultsDTO ctrMock = new CrfRatingResultsDTO();
-ctrMock.setFullModelProbabilityOfDefault(BigDecimal.valueOf(2.69));
-ctrMock.setFullModelRating("7");
-ctrMock.setFullModelRatingDate(LocalDate.parse("2026-06-01"));
+ctrMock.setFullModelProbabilityOfDefault(BigDecimal.valueOf(0.0200));
+ctrMock.setFullModelRating("2+");
+ctrMock.setFullModelRatingDate(LocalDate.parse("2026-05-28"));
 ctrMock.setIsMRCCapApplied(false);
 
 UpwardAndDownwardOverrideResultsDTO overrideMock = new UpwardAndDownwardOverrideResultsDTO();
-overrideMock.setUpwardOverride(List.of("6+ (0,58%)", "6+ (0,81%)", "6-"));
-overrideMock.setDownwardOverride(List.of("7-", "8+", "8"));
+overrideMock.setUpwardOverride(List.of("1"));
+overrideMock.setDownwardOverride(List.of("3-", "4-", "5"));
 
-RatingResultsWithUpwardAndDownwardOverrideDTO mock = new RatingResultsWithUpwardAndDownwardOverrideDTO();
-mock.setCrfRatingResults(ctrMock);  // ← crf pas ctr
+RatingResultsWithOverrideFields mock = new RatingResultsWithOverrideFields();
+mock.setCrfRatingResults(ctrMock);
 mock.setUpwardAndDownwardOverrideResultsDTO(overrideMock);
 
 return mock;
+
 
