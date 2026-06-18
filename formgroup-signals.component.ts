80a6a5aaae8ff6702fbc77f10698af3a6822e936
Angular2026 <bouhmid@me.com>
2026-06-18 02:01:52 +0200

    Update formgroup-signals.component.ts

--- formgroup-signals.component.ts
+++ formgroup-signals.component.ts
@@ -1,45 +1,8 @@
-// remove: private readonly invalidSubsidiary = (control: AbstractControl) => ({invalidSubsidiary: true});
+private readonly lboErrors: SubsidiaryValidationErrors[] = [
+  'CRF_FLAGGED_LBO',
+  'RMPM_FLAGGED_LBO',
+];
 
-private currentSubsidiaryValidator: ValidatorFn | null = null;
-
-private buildInvalidSubsidiaryValidator(alertTextIds: string[]): ValidatorFn {
-  return (): ValidationErrors | null => {
-    const errors: ValidationErrors = {};
-    alertTextIds.forEach(id => (errors[id] = true));
-    return errors;
-  };
-}
-
-
-if (response.errorsList.length > 0) {
-  this.handlePropagationErrors(selectValue, response);
-  const alertTextIds = response.errorsList.map(error => error.alertTextId);
-
-  response.errorsList.forEach(error => {
-    this.workflowValidationService.addWorkflowAlerts('errors', [
-      {
-        alertTextId: error,
-        anchorId: 'rmpmidForInheritanceSearch',
-        fragmentId: 'counterpartyRating',
-      },
-    ]);
-  });
-
-  this.isSubsidiaryCounterpartyValidated = false;
-  this.propagationService.updateSubsidiaryEntitySelectionValidity(this.isSubsidiaryCounterpartyValidated);
-
-  this.currentSubsidiaryValidator = this.buildInvalidSubsidiaryValidator(alertTextIds);
-  control.addValidators([this.currentSubsidiaryValidator]);
-  control.updateValueAndValidity();
-} else {
-  this.isSubsidiaryCounterpartyValidated = true;
-  this.propagationService.updateSubsidiaryEntitySelectionValidity(this.isSubsidiaryCounterpartyValidated);
-  // ...selectedSubsidiary payload unchanged...
-  this.setSubsidiaryDetails(selectedSubsidiary);
-
-  if (this.currentSubsidiaryValidator) {
-    control.removeValidators([this.currentSubsidiaryValidator]);
-    this.currentSubsidiaryValidator = null;
-    control.updateValueAndValidity();
-  }
+private prefixLboError(alertTextId: SubsidiaryValidationErrors): string {
+  return this.lboErrors.includes(alertTextId) ? `SUBS_${alertTextId}` : alertTextId;
 }
