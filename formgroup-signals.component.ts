c8c1ae933edfb1dcfc7b61daac92dbdb7f7e08da
Angular2026 <bouhmid@me.com>
2026-06-18 02:56:29 +0200

    Update formgroup-signals.component.ts

--- formgroup-signals.component.ts
+++ formgroup-signals.component.ts
@@ -1,8 +1,1 @@
-private readonly lboErrors: SubsidiaryValidationErrors[] = [
-  'CRF_FLAGGED_LBO',
-  'RMPM_FLAGGED_LBO',
-];
 
-private prefixLboError(alertTextId: SubsidiaryValidationErrors): string {
-  return this.lboErrors.includes(alertTextId) ? `SUBS_${alertTextId}` : alertTextId;
-}
