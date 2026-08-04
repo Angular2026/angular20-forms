5346a068209a00a83ffa0e8d9f46de1c092c0490
Angular2026 <bouhmid@me.com>
2026-08-04 15:36:12 +0200

    Update formgroup-signals.component.ts

--- formgroup-signals.component.ts
+++ formgroup-signals.component.ts
@@ -1,13 +1,14 @@
+private workflowDTOLocal = linkedSignal(() => this.workflowDTO());
+
+
 .subscribe(response => {
-  // refresh new rating in window history
-  this.workflowDTO.update(dto => ({ ...dto, counterPartyRating: response }));
-  const newWorkflow = this.workflowDTO(); // nouvelle référence, propre
+  this.workflowDTOLocal.update(dto => ({ ...dto, counterPartyRating: response }));
+  const newWorkflow = this.workflowDTOLocal();
 
   this.router.navigate([`/counterparty/detail/${this.rmpmId()}/workflows`], {
     state: newWorkflow,
     queryParams: { workflowId: newWorkflow.encryptedUUID, category: newWorkflow.category?.map(c => c.value) },
     onSameUrlNavigation: 'ignore',
   });
-  // clear errors box
   this.alertsBoxService.setSectionValid('counterpartyRating');
 });
