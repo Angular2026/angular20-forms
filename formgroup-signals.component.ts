a043174ac50a450f499c890be5a9c5091c7aed5f
Angular2026 <bouhmid@me.com>
2026-08-04 15:37:53 +0200

    Update formgroup-signals.component.ts

--- formgroup-signals.component.ts
+++ formgroup-signals.component.ts
@@ -10,3 +10,16 @@ private syncEffect = effect(() => {
 
 // accès readonly pour le reste du composant
 workflowDTOLocal = this._workflowDTOLocal.asReadonly();
+
+
+.subscribe(response => {
+  this._workflowDTOLocal.update(dto => ({ ...dto, counterPartyRating: response }));
+  const newWorkflow = this._workflowDTOLocal();
+
+  this.router.navigate([`/counterparty/detail/${this.rmpmId()}/workflows`], {
+    state: newWorkflow,
+    queryParams: { workflowId: newWorkflow.encryptedUUID, category: newWorkflow.category?.map(c => c.value) },
+    onSameUrlNavigation: 'ignore',
+  });
+  this.alertsBoxService.setSectionValid('counterpartyRating');
+});
