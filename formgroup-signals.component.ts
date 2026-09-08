6cc18719fa46c30c070542c3850f87878bc822bf
Angular2026 <bouhmid@me.com>
2026-09-08 13:41:21 +0200

    Update formgroup-signals.component.ts

--- formgroup-signals.component.ts
+++ formgroup-signals.component.ts
@@ -1,25 +1,7 @@
-workflowDTO = input.required<WorkflowDocument>(); // input existant, inchangé
-
-// copie locale writable
-private _workflowDTOLocal = signal<WorkflowDocument>(this.workflowDTO());
-
-// se resynchronise si le parent repasse une nouvelle référence
-private syncEffect = effect(() => {
-  this._workflowDTOLocal.set(this.workflowDTO());
-}, { allowSignalWrites: true });
-
-// accès readonly pour le reste du composant
-workflowDTOLocal = this._workflowDTOLocal.asReadonly();
-
-
-.subscribe(response => {
-  this._workflowDTOLocal.update(dto => ({ ...dto, counterPartyRating: response }));
-  const newWorkflow = this._workflowDTOLocal();
-
-  this.router.navigate([`/counterparty/detail/${this.rmpmId()}/workflows`], {
-    state: newWorkflow,
-    queryParams: { workflowId: newWorkflow.encryptedUUID, category: newWorkflow.category?.map(c => c.value) },
-    onSameUrlNavigation: 'ignore',
-  });
-  this.alertsBoxService.setSectionValid('counterpartyRating');
-});
+this.workflowService.getFrbData(this.workflowDTO()).pipe(
+  tap(frb => this.isFrbRatingPerimeter.set(frb.frbRatingPerimeter)),
+  filter(() => this.isFrbRatingPerimeter()),
+  switchMap(() => this.workflowService.fetchRatingPolicyDetails(dto)),
+  switchMap(details => { this.applyDetails(details); return this.workflowService.getSRPlist(true); }),
+  takeUntilDestroyed(this.destroyRef)
+).subscribe(srplist => this.mapSrpList(srplist));
