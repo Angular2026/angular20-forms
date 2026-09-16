f9c9b64801cba22b198ae65082b1f0cd08228e89
Angular2026 <bouhmid@me.com>
2026-09-16 04:08:59 +0200

    Update signal-formgroup.component.ts

--- signal-formgroup.component.ts
+++ signal-formgroup.component.ts
@@ -1,7 +1,9 @@
-SELECT dc.id, w.encrypted_uuid, wh.current_state, dc.created_timestamp
-FROM defaulting_clients dc
-LEFT JOIN workflow_document w ON w.defaulting_client_id = dc.id
-LEFT JOIN workflow_history wh ON wh.workflow_id = w.id
-WHERE dc.counterparty_id = 72696
-ORDER BY dc.id;
+Bonjour Frédéric,
+
+Je voulais te prévenir que je suis en arrêt maladie jusqu’au 16 inclus. Je devrais être de retour demain.
+
+Petit update sur mon ticket XXXX :
+
+* La partie backend est terminée et déjà incluse dans la branche de Widad. Elle est en train de faire les tests, puis ce sera déployé dès qu’elle aura terminé.
+* La partie frontend est déjà mergée, mais elle ne peut pas encore être testée tant que le backend n’est pas déployé.
 
