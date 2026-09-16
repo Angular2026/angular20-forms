a7638f79ce3a086e314acf348d31838643ccd103
Angular2026 <bouhmid@me.com>
2026-09-16 04:13:10 +0200

    Update signal-formgroup.component.ts

--- signal-formgroup.component.ts
+++ signal-formgroup.component.ts
@@ -7,3 +7,11 @@ Petit update sur mon ticket XXXX :
 * La partie backend est terminée et déjà incluse dans la branche de Widad. Elle est en train de faire les tests, puis ce sera déployé dès qu’elle aura terminé.
 * La partie frontend est déjà mergée, mais elle ne peut pas encore être testée tant que le backend n’est pas déployé.
 
+Bonjour Frédéric,
+
+Je voulais te prévenir que je suis en arrêt maladie jusqu’au 16 inclus. Je devrais être de retour demain.
+
+Petit update sur mon ticket XXXX :
+
+* Les développements backend sont terminés. Comme il y a une dépendance avec le ticket de Widad, ils ont été intégrés dans sa branche et seront déployés dès qu’elle aura terminé.
+* La partie frontend est déjà mergée, mais elle ne peut pas encore être testée tant que le backend n’est pas déployé.
