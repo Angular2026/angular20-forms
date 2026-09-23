04926c2fa3499f3a4d08f0f5054bc869cadc3c71
Angular2026 <bouhmid@me.com>
2026-09-23 15:03:42 +0200

    Update signal-formgroup.component.ts

--- signal-formgroup.component.ts
+++ signal-formgroup.component.ts
@@ -1,16 +1,18 @@
-public SRPDecisionTreeArchive(SRPDecisionTree srpDecisionTree) {
-    this.questions = nonNull(srpDecisionTree) ? srpDecisionTree.getQuestions() : null;
+Demande de badge d’accès – Mellinaire 1
 
-    String srp;
-    if (nonNull(srpDecisionTree)) {
-        if (!nonNull(srpDecisionTree.getSrpResponseCombination())) {
-            srp = srpDecisionTree.getWorkflowDocument().getCounterPartyRating().getXxx();
-        } else {
-            srp = srpDecisionTree.getSrpResponseCombination().getRecommendedSRP().getXxx();
-        }
-    } else {
-        srp = null;
-    }
+Bonjour,
 
-    this.recommendedSRP = srp;
-}
+Je souhaite faire une demande de badge afin d’avoir accès au bâtiment Mellinaire 1. Étant actuellement rattaché à Valmy 1, j’aurais quelques questions avant de valider la demande.
+
+J’ai déjà reçu un nouveau badge, mais celui-ci me permet uniquement d’accéder à Mellinaire 1 et à la cantine. Une fois à l’intérieur du bâtiment, je ne peux pas badger pour accéder aux différentes zones.
+
+Je voulais donc savoir :
+
+* Quel statut dois-je sélectionner dans le formulaire ? Je suis prestataire et j’occupe un poste de développeur logiciel.
+* J’utilise toujours mon ancien badge avec la puce Smartcard pour me connecter à mon PC. Est-ce qu’une nouvelle demande de badge risque de désactiver ou d’impacter ce badge ?
+* Le formulaire indique que le badge doit être récupéré à Valmy 1. Serait-il possible de le récupérer directement à Mellinaire 1 ? À défaut, est-ce qu’un collègue peut le récupérer à Valmy 1 à ma place ?
+
+Merci par avance pour votre aide.
+
+Bien cordialement,
+Ahmed
