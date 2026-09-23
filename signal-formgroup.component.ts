47043fe595ff2989833fa44f3e6d78b9fb638b4c
Angular2026 <bouhmid@me.com>
2026-09-23 16:53:42 +0200

    Update signal-formgroup.component.ts

--- signal-formgroup.component.ts
+++ signal-formgroup.component.ts
@@ -1,16 +1,14 @@
-Demande de badge d’accès – Mellinaire 1
+Changement de lieu de rattachement – Valmy 1 vers Mellinaire 1
 
 Bonjour,
 
-Je souhaite faire une demande de badge afin d’avoir accès au bâtiment Mellinaire 1. Étant actuellement rattaché à Valmy 1, j’aurais quelques questions avant de valider la demande.
+Je vous contacte afin de connaître la procédure à suivre pour modifier mon lieu de rattachement, actuellement enregistré à Valmy 1, vers Mellinaire 1.
 
-J’ai déjà reçu un nouveau badge, mais celui-ci me permet uniquement d’accéder à Mellinaire 1 et à la cantine. Une fois à l’intérieur du bâtiment, je ne peux pas badger pour accéder aux différentes zones.
+Je dois effectuer une demande de remplacement de mon badge perdu et je souhaiterais que le nouveau badge puisse être livré / mis à disposition à Mellinaire 1, plutôt qu’à Valmy 1.
 
-Je voulais donc savoir :
+Pouvez-vous m’indiquer la démarche à suivre pour effectuer ce changement de rattachement avant que je fasse la demande de badge ?
 
-* Quel statut dois-je sélectionner dans le formulaire ? Je suis prestataire et j’occupe un poste de développeur logiciel.
-* J’utilise toujours mon ancien badge avec la puce Smartcard pour me connecter à mon PC. Est-ce qu’une nouvelle demande de badge risque de désactiver ou d’impacter ce badge ?
-* Le formulaire indique que le badge doit être récupéré à Valmy 1. Serait-il possible de le récupérer directement à Mellinaire 1 ? À défaut, est-ce qu’un collègue peut le récupérer à Valmy 1 à ma place ?
+Je mets mon manager en copie pour information.
 
 Merci par avance pour votre aide.
 
